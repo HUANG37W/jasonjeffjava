d10cec7399504ce4588d053826d314ba6449b2ab
Jason Huang <huang78@byui.edu>
2023-09-07 22:06:06 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Select the HTML element to manipulate
+const myHeading = document.querySelector('h1');
+// Manipulate the HTML element
+myHeading.textContent = 'Hello world!';
+// Try to complete the method with options
+myHeading.textContent = 'Hello world!'.toLowerCase();
+
+
+
